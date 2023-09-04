c06b023be39a2096296fd0a5bcc2991600a4525f
adriel68 <adrielsanchez68@outlook.com>
2023-09-04 10:15:47 -0700

    Ej:1 detalles / mejoras

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function mostrarMensajeInicial() {
+
+    if (confirm('Este sitio esta en desarrollo, disculpe las molestias')) {
+      alert('Gracias por visitarnos!');
+    } else {
+      alert('...');
+    }
+  
+  }
